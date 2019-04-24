6f7cdcdae7a3241df824a8be52096413d34f2bd5
Ashley Miller <asm2fe@virginia.edu>
2019-04-24 21:58:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,14 @@
 class Triangle
 
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(l, m , n)
+    @s1 = l
+    @s2 = m
+    @s3 = n
+  end
+  def kind
+  end
   class TriangleError < StandardError
     def message
       "Triangles must consist of sides whose lengths are > 0 additionally,
