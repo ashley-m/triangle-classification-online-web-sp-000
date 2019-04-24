52321adeecf2645944dcdc1d19e136c658e7e974
Ashley Miller <asm2fe@virginia.edu>
2019-04-24 22:13:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,27 @@ class Triangle
     @s2 = m
     @s3 = n
   end
+  
   def kind
+    if @s1 == 0 || @s2 == 0 || @s3 == 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    else
+      case 
+      when @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+        :isosceles
+      when @s1 == @s2 && @s1 == @s3
+        :equilateral
+      when @s1 != @s2 && @s1 != @s3 && @s2 != @s3
+        :scalene
+      end
+    end
   end
+
+  def 
   class TriangleError < StandardError
     def message
       "Triangles must consist of sides whose lengths are > 0 additionally,
