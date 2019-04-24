002eb2653142f17ae291438e63b115e62ccec237
Ashley Miller <asm2fe@virginia.edu>
2019-04-24 21:55:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,8 @@
 class Triangle
-  # write code here
+
+  class TriangleError < StandardError
+    def message
+      "Triangles must consist of sides whose lengths are > 0 additionally,
+      no side must exceed the sum of the other two sides!"
+  end
 end
